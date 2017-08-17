b2189daee777af907b2e5a2edde235fa3c0c33c3
joshcamp503 <joshcamp503@gmail.com>
2017-08-17 14:58:32 -0700

    Intial commit with first chunk of app done; lacks functionality

---
+++ script.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+   $('div').click(function() {
+      alert($(this).text());
+   }); 
+});
+
+function add(a, b){
+  console.log(a + b);
+}
+
+add(3,2)
+
+function subtract(a, b){
+  console.log(a - b);
+}
+
+subtract(3,2)
+
+function multiply(a, b){
+  console.log(a * b);
+}
+
+multiply(3,2)
+
+function divide(a, b){
+  console.log(a / b);
+}
+
+
+divide(3,2)
+
